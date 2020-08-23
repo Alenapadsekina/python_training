495562f289e41235d319af2def07ac06f8608a6e
Alenapadsekina <alena.padsekina@gmail.com>
2020-08-23 14:30:39 +0300

    refactor: add parameters from contact.py

--- add_new_contact.py
+++ add_new_contact.py
@@ -15,11 +15,12 @@ class AddNewContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd)
-        self.create_new_contact(wd, Contact(first_name="first_name", middle_name="middle_name", last_name="last_name",
+        self.create_new_contact(wd, Contact(first_name="first_name", middle_name="middle_name", last_name="last_name", nickname="nickname",
                            company="some_company", address_1="123 Tice Blvd.", mobile_phone="2013912500",
                            address_2="Woodcliff Lake", home_phone="2013912501", work_phone="2013912502", fax="2013912503",
                            email_2="test.email.2@test.com", email_3="test.email.3@test.com",
-                           email_1="test.email.1@test.com", website="http://homepage.com"))
+                           email_1="test.email.1@test.com", website="http://homepage.com", group_phone="8885016953", birth_day="17", birth_month="February", anniversary_date="18",
+                           anniversary_month="December", anniversary_year="1990", birth_year="1990"))
         self.logout(wd)
 
     def create_new_contact(self, wd, contact):
@@ -35,7 +36,7 @@ class AddNewContact(unittest.TestCase):
         wd.find_element_by_name("lastname").send_keys(contact.last_name)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("nickname")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         # company
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
@@ -75,29 +76,29 @@ class AddNewContact(unittest.TestCase):
         wd.find_element_by_name("homepage").send_keys(contact.website)
         # dates
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("17")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birth_day)
         wd.find_element_by_name("bday").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("February")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birth_month)
         wd.find_element_by_name("bmonth").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1990")
+        wd.find_element_by_name("byear").send_keys(contact.birth_year)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("18")
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.anniversary_date)
         wd.find_element_by_name("aday").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("December")
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.anniversary_month)
         wd.find_element_by_name("amonth").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("1990")
+        wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
         # group
         wd.find_element_by_name("new_group").click()
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("8885016953")
+        wd.find_element_by_name("phone2").send_keys(group_phone)
         # notes
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
--- contact.py
+++ contact.py
@@ -1,8 +1,9 @@
 class Contact:
-    def __init__(self, first_name, middle_name, last_name, company, address_1, mobile_phone, address_2, home_phone,work_phone, fax, email_1, email_2, email_3, website):
+    def __init__(self, first_name, middle_name, last_name, nickname, company, address_1, mobile_phone, address_2, home_phone,work_phone, fax, email_1, email_2, email_3, website, group_phone, birth_day, birth_month, anniversary_date, anniversary_month, anniversary_year, birth_year):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
+        self.nickname = nickname
         self.company = company
         self.address_1 = address_1
         self.mobile_phone = mobile_phone
@@ -15,3 +16,10 @@ class Contact:
         self.email_2 = email_2
         self.email_3 = email_3
         self.website = website
+        self.group_phone = group_phone
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
+        self.anniversary_date = anniversary_date
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
