7427c88c89dcdb3cd2d9470c96ccfe131c5c18b3
alena.padsekina <alena.padsekina@gmail.com>
2020-09-08 18:03:24 +0300

    add check: old_gr_length>new_gr_length

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -4,9 +4,15 @@ from model.group import Group
 
 # ADD A NEW GROUP
 def test_add_new_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new_group(Group(name="new group", header="group header", footer="group footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)+1 == len(new_groups)
 
 
 # ADD EMPTY GROUP
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new_group(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)+1 == len(new_groups)
--- test/test_delete_1st_group.py
+++ test/test_delete_1st_group.py
@@ -2,9 +2,14 @@ from model.group import Group
 
 # DELETE 1ST GROUP
 def test_delete_1st_group(app):
+
     if app.group.count() == 0:
         app.group.create_new_group(Group(name="new group", header="group header", footer="group footer"))
         print("NO GROUPS")
     else:
+        old_groups = app.group.get_group_list()
         app.group.delete_first_group()
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)-1 == len(new_groups)
