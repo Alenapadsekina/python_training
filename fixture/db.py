e253817ad7530f45ebf0bc428c1eaf5cd078c61a
alena.padsekina <alena.padsekina@gmail.com>
2020-10-19 15:02:51 +0300

    compare db to ui (contacts from home page)

--- fixture/db.py
+++ fixture/db.py
@@ -26,10 +26,12 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, home, mobile, work, email, email2, email3 from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id = str(id), first_name=firstname, last_name=lastname))
+                (id, firstname, lastname, home, mobile, work, email, email2, email3) = row
+                list.append(Contact(id = str(id), first_name=firstname, last_name=lastname,
+                                    all_phones_from_contact_page = home+'\n'+mobile+'\n'+work,
+                                    all_emails_from_contact_page = email+'\n'+email2+'\n'+email3))
         finally:
             cursor.close()
         return list
--- model/contact.py
+++ model/contact.py
@@ -35,7 +35,7 @@ class Contact:
 
 
     def __repr__(self):
-        return "%s:%s %s" % (self.id, self.last_name, self.first_name)
+        return "%s:%s %s %s %s" % (self.id, self.last_name, self.first_name, self.all_phones_from_contact_page, self.all_emails_from_contact_page)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.last_name == other.last_name and self.first_name == other.first_name
