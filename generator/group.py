875ba9346aa36a8ce1cee936134fef67afd93aa9
alena.padsekina <alena.padsekina@gmail.com>
2020-10-21 14:44:32 +0300

    fix testdata

--- generator/group.py
+++ generator/group.py
@@ -27,16 +27,12 @@ def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + ' '
     return prefix + "".join(random.choice(symbols) for i in range(random.randrange(maxlen)))
 
-#testdata = [Group(name="", header="", footer="")] + [
-#    Group(name=random_string("name_", 10), header=random_string("header_", 10), footer=random_string("footer_", 10))
-#    for i in range(5)
-#    ]
-
-testdata = [Group(name=name, header=header, footer=footer)
-    for name in ['', random_string("name_", 10)]
-    for header in ['', random_string("header_", 10)]
-    for footer in ['', random_string("footer_", 10)]
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name_", 10), header=random_string("header_", 10), footer=random_string("footer_", 10))
+    for i in range(5)
     ]
+
+
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, 'w') as out:
